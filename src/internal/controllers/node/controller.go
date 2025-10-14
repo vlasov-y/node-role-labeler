d5b182b868eb741941bd9190d0de9d2a2c3af131
Yurii Vlasov <yuriy@vlasov.pro>
2025-10-14 13:40:05 +0300

    fixing lint and licenes issues
    Signed-off-by: Yurii Vlasov <yuriy@vlasov.pro>

--- src/internal/controllers/node/controller.go
+++ src/internal/controllers/node/controller.go
@@ -1,19 +1,18 @@
-/*
-Copyright 2025.
+// Copyright 2025 The Node Role Labeler Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
+// Package node contains the controller that actually manages node role labels
 package node
 
 import (
@@ -42,6 +41,7 @@ type NodeReconciler struct {
 // +kubebuilder:rbac:groups="",resources=nodes/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups="",resources=events,verbs=create;get;list;patch;update;watch
 
+//nolint:gocyclo
 func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
 	log := logf.FromContext(ctx)
 
